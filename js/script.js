7db7805f84c63ffeb293cbbbb5764e7ac7615ee2
Anna Kijania <kijania.anna@gmail.com>
2020-06-11 10:23:17 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@
 //   });
 
 const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event)
-    /* remove class 'active' from all article links  */
+    /* [DONE]remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
@@ -15,8 +16,10 @@ const titleClickHandler = function (event) {
     }
 
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE]remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.active.active');
 
     for(let activeArticle of activeArticles){
